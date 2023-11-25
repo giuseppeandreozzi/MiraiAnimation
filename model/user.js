b0ffe050a54d36f52be1d460250e25804502e2f2
giuseppeandreozzi <g.andreozzi98@gmail.com>
2023-11-25 11:23:10 +0100

    Ultimati i model

--- model/user.js
+++ model/user.js
@@ -22,7 +22,41 @@ const userSchema = new Schema({
     tipo: {
         type: String,
         required: true
-    }
-});
+    },
+    carrello: [{
+        quantity: {
+            type: Number,
+            required: true
+        },
+        prodotto: {
+            type: Schema.Types.ObjectId,
+            ref: "Blu-ray",
+            required: true
+        },
+        prezzo: {
+            type: Number,
+            required: true
+        }
+    }],
+    ordini: [{
+        quantity: {
+            type: Number,
+            required: true
+        },
+        prodotto: {
+            type: Schema.Types.ObjectId,
+            ref: "Blu-ray",
+            required: true
+        },
+        prezzo: {
+            type: Number,
+            required: true
+        }
+        }],
+        prezzoTotale: {
+            type: Number,
+            required: true
+        }
+    });
 
 export default model("User", userSchema);
